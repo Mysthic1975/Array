f33f191177333ac699bff72eb29cd672a96a5c0e
Mysthic1975 <108031921+Mysthic1975@users.noreply.github.com>
2023-04-03 15:26:56 +0200

    0.0.2

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,69 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
+        char a = '+';
+        char b = '#';
+        char c = '%';
+        char d = '-';
         char[] Zeichen;
-        char[] NummerZwei = {'+','#','%'};
+        char[] NummerZwei = {a, b, c, d, 'a', 'b', 'c', 'd'};
         boolean Gleicheit = true;
         Scanner s = new Scanner(System.in);
         int Groesse;
         String st;
 
         System.out.println("Bitte gebe die größe deines Arrays an");
+        while (!s.hasNextInt()) {
+            System.out.println("Ungültige Eingabe. Bitte geben Sie eine gültige Ganzzahl ein:");
+            s.next();
+        }
         Groesse = s.nextInt();
         Zeichen = new char[Groesse];
 
-        for(int i =0; i < Groesse; i++)
-        {
-            System.out.println("Hallo Benutzer gib deine "+ i +" Zahl an");
+        for (int i = 0; i < Groesse; i++) {
+            System.out.println("Hallo Benutzer gib deine " + i + " Zahl an");
             st = s.next();
-            Zeichen[i]= st.charAt(0);
+            Zeichen[i] = st.charAt(0);
         }
 
-        for(int i=0 ; i<Groesse ; i++) // i++ == i = i + 1 == i += 1
-        {
-            System.out.println(Zeichen[i]);
-            //System.out.println(i);
+        for (char zeichen : Zeichen) {
+            System.out.println(zeichen);
         }
 
         int minGroesse = Math.min(NummerZwei.length, Groesse);
-        for(int i=0;i<minGroesse;i++)
-        {
-            if(NummerZwei[i] != Zeichen[i])
-            {
+        for (int i = 0; i < minGroesse; i++) {
+            if (NummerZwei[i] != Zeichen[i]) {
                 System.out.println("Die Arrays sind nicht gleich, an der Stelle: " + i);
                 Gleicheit = false;
             }
         }
 
-        if(Gleicheit)
+        if (Gleicheit)
             System.out.println("Die Arrays sind gleich");
 
-        // Erweiterung: Überprüfen Sie, ob ein vom Benutzer angegebenes Zeichen im Array vorhanden ist
-        System.out.println("Bitte geben Sie ein Zeichen ein, um zu überprüfen, ob es im Array vorhanden ist:");
-        char benutzerZeichen = s.next().charAt(0);
-        boolean gefunden = false;
-        int position = -1;
-
-        for (int i = 0; i < Groesse; i++) {
-            if (Zeichen[i] == benutzerZeichen) {
-                gefunden = true;
-                position = i;
-                break;
+        // Erweiterung: Überprüfen Sie, ob ein bestimmtes Zeichen im Array vorhanden ist
+        char[] zuSuchendeZeichen = {'a', 'b', 'c', 'd'};
+        for (char zeichen : zuSuchendeZeichen) {
+            if (istZeichenImArrayEnthalten(Zeichen, zeichen)) {
+                System.out.println("Das Zeichen " + zeichen + " ist im Array enthalten.");
+            } else {
+                System.out.println("Das Zeichen " + zeichen + " ist nicht im Array enthalten.");
             }
         }
 
-        if (gefunden) {
-            System.out.println("Das Zeichen " + benutzerZeichen + " wurde im Array an der Position " + position + " gefunden.");
-        } else {
-            System.out.println("Das Zeichen " + benutzerZeichen + " wurde nicht im Array gefunden.");
+        // Ergebnisse als Paare anzeigen
+        for (int i = 0; i < minGroesse; i++) {
+            System.out.println(Zeichen[i] + " - " + NummerZwei[i]);
         }
+    }
+
+    public static boolean istZeichenImArrayEnthalten(char[] array, char zeichen) {
+        for (char element : array) {
+            if (element == zeichen) {
+                return true;
+            }
+        }
+        return false;
     }
 }
